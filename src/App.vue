1a85f42727a5fc857c5a24fcf396115a7a11e4cf
Danyvi <vinci.dani@gmail.com>
2023-12-06 23:35:43 -0500

    design

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,19 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink> |
-  <RouterLink to="/stats">Stats</RouterLink>
-  <RouterView />
+  <NavBar />
+
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
 
 <script setup>
+/*
+  imports
+*/
 
+  import NavBar from '@/components/layouts/NavBar.vue'
 </script>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
