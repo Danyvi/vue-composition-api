b55d61339ce8b06a0e28b2403d562e2e78f19878
Danyvi <vinci.dani@gmail.com>
2023-12-06 22:45:11 -0500

    added router

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from '@/router'
+
+
+createApp(App)
+  .use(router)
+  .mount('#app')
