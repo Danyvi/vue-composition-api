54f9f172a6faf01984da03a9b9aacc40614e272c
Danyvi <vinci.dani@gmail.com>
2023-12-07 21:25:30 -0500

    notes add note method

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -5,15 +5,21 @@
       <div class="field">
         <div class="control">
           <textarea
+            v-model="newNote"
             class="textarea"
             placeholder="Add a new note"
+            ref="newNoteRef"
           />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">
+          <button
+            class="button is-link has-background-success"
+            @click="addNote"
+            :disabled="newNote.length < 5"
+          >
             Add New Note
           </button>
         </div>
@@ -21,12 +27,13 @@
     </div>
 
     <div
-      v-for="i in 3"
+      v-for="note in notes"
+      :key="note.id"
       class="card mb-4"
     >
       <div class="card-content">
         <div class="content">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.
+          {{ note.content }}
         </div>
       </div>
       <footer class="card-footer">
@@ -36,3 +43,44 @@
     </div>
   </div>
 </template>
+
+<script setup>
+/**
+ * Imports
+ */
+
+ import { ref } from 'vue';
+
+
+/**
+ * Notes
+ */
+
+const newNote = ref("")
+const newNoteRef = ref(null)
+
+const notes = ref([
+  {
+    id: 'id1',
+    content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ipsa commodi sint ut ullam culpa nulla molestiae sunt quia qui maxime, enim quasi officiis aperiam fugit, corrupti omnis, eaque animi.'
+  },
+  {
+    id: 'id2',
+    content: 'This is a shorter note'
+  }
+])
+
+const addNote = () => {
+
+  const note = {
+    id: new Date().getTime().toString(),
+    content: newNote.value
+  }
+
+  notes.value.unshift(note)
+
+  newNote.value = ''
+
+  newNoteRef.value.focus()
+}
+</script>
