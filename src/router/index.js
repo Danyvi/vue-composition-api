8952fac758dad6133e5c7d25fd4164206f3f3d2e
Danyvi <vinci.dani@gmail.com>
2023-12-17 21:49:53 -0500

    navigation guards

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import ViewNotes from '@/views/ViewNotes.vue'
 import ViewEditNote from '@/views/ViewEditNote.vue'
 import ViewStats from '@/views/ViewStats.vue'
 import ViewAuth from '@/views/ViewAuth.vue'
+import { useStoreAuth } from '@/stores/storeAuth'
+
 
 const routes = [
   {
@@ -30,6 +32,41 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+// Navigation Guards
+router.beforeEach(async (to, from) => {
+
+  const storeAuth = useStoreAuth()
+
+  // if you are not logged in and want to access any route that is not 'auth' you will be forbidden
+  if (
+    !storeAuth.user.id &&
+    to.name !== 'auth'
+  ) {
+    return {
+      name: 'auth'
+    }
+  }
+
+  if(
+    storeAuth.user.id &&
+    to.name === 'auth'
+  ) {
+    // prevent the user to leave the current route
+    return false
+  }
+
+  // if (
+  //   // make sure the user is authenticated
+  //   !isAuthenticated &&
+  //   // ❗️ Avoid an infinite redirect
+  //   to.name !== 'Login'
+  // ) {
+  //   // redirect the user to the login page
+  //   return { name: 'Login' }
+  // }
+  console.log(`from: ${from.path} --> to: ${to.path}`);
 })
 
 export default router
